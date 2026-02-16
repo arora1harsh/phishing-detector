8f3eacc12d4b699968b3f947ab20e2e17a24fff7
Harsh Arora <arora1harsh@gmail.com>
2026-02-16 15:50:56 +0530

    Improved weighted phishing detection algorithm

--- backend/app.py
+++ backend/app.py
@@ -5,28 +5,38 @@ import re
 app = Flask(__name__)
 CORS(app)
 
-# Basic phishing detection rules
 def calculate_risk(url):
     score = 0
 
     # Rule 1: Very long URL
     if len(url) > 75:
-        score += 1
+        score += 2
 
     # Rule 2: Contains @ symbol
     if "@" in url:
-        score += 1
+        score += 3
 
     # Rule 3: Too many dots
     if url.count('.') > 3:
-        score += 1
+        score += 2
 
     # Rule 4: IP address instead of domain
     ip_pattern = r"(\d{1,3}\.){3}\d{1,3}"
     if re.search(ip_pattern, url):
-        score += 1
+        score += 3
+
+    # Rule 5: Suspicious keywords
+    suspicious_keywords = [
+        "login", "verify", "update", "secure",
+        "bank", "account", "confirm", "password"
+    ]
+
+    for keyword in suspicious_keywords:
+        if keyword in url.lower():
+            score += 1
 
     return score
+
 
 
 @app.route("/check-url", methods=["POST"])
@@ -39,10 +49,13 @@ def check_url():
 
     risk_score = calculate_risk(url)
 
-    if risk_score >= 2:
+    if risk_score >= 5:
+        verdict = "High Risk"
+    elif risk_score >= 2:
         verdict = "Suspicious"
     else:
         verdict = "Safe"
+
 
     return jsonify({
         "risk_score": risk_score,
